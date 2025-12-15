097ccfd758c18d5916019397a5eed09e8bf636a5
Luiz Eduardo da Silva Alverga <luizzz154@hotmail.com>
2025-12-14 23:43:42 -0300

    adicionado suspense por volta do componente de aulas

--- app/(erp)/aulas/page.tsx
+++ app/(erp)/aulas/page.tsx
@@ -1,10 +1,15 @@
 import { ClassroomsList } from '@/components/classrooms/classrooms-list'
 import { Metadata } from 'next'
+import { Suspense } from 'react'
 
 export const metadata: Metadata = {
   title: 'Aulas',
 }
 
 export default function ClassroomsPage() {
-  return <ClassroomsList />
+  return (
+    <Suspense>
+      <ClassroomsList />
+    </Suspense>
+  )
 }
